2b4c96b5bcaf4b1bc0216f017c2d426b7c905011
Kevin O'Leary <olearyk57@gmail.com>
2021-10-07 11:23:30 -0400

    Classes created

---
+++ dotnet/Capstone/Vending Machine Items/Gum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Vending_Machine_Items
+{
+    class Gum : IVendingMachineItem
+    {
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int ItemAmountInInventory { get; set; }
+        public Gum(string itemName, decimal itemPrice, int itemAmountInInventory)
+        {
+            this.ItemName = itemName;
+            this.ItemPrice = itemPrice;
+            this.ItemAmountInInventory = itemAmountInInventory;
+
+        }
+    }
+}
---
+++ dotnet/Capstone/Vending Machine Items/IVendingMachineItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Vending_Machine_Items
+{
+    interface IVendingMachineItem
+    {
+    }
+}
